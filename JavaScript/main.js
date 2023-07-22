923d43abdad413109af3e88f1d29692f752cf86b
vishal-038 <vk067414@gmail.com>
2023-07-22 10:03:54 +0530

    array

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -57,3 +57,91 @@ demo2>=0 ? console.log(demo2 + " demo is a +ve number"):console.log(demo2 +" dem
 
 demo2=-10;
 demo2>=0 ? console.log(`${demo2} is a +ve number`):console.log(`${demo2} is a -ve number`);
+
+//let, var , const -> keyword of java script
+//let is used to only access the data within the scope.(private access)
+//var is used to acess the data anywhere where we need.(public access)
+//const .the value inside this is constant can not change the value. it must be initialize while declaring.(private access)
+Flag = true;
+if(Flag){
+    let a
+    a=10;
+    var b
+    b=12;
+    const c=110;
+    // c=250; //error
+    //let a=100;
+    console.log(`the value of a inside the block is ${a}`)
+    console.log(`the value of b inside the block is ${b}`)
+    console.log(`the value of c inside the block is ${c}`)
+}
+else{
+    let b=123100
+    console.log(`the value inside the block is ${b}`)
+}
+console.log(`a outside block is ${b}`)
+//console.log(`b outside block is ${a}`) //error not defined
+//console.log(`c outside block is ${c}`) //error not defined
+
+
+//loops :- for , while
+// the loops will generate value
+console.log("loop for")
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log("while loop")
+let i=0;
+while (i<5) {
+    console.log(i);
+    i++;
+}
+
+//non primitve data type
+// arrays []->identify by this , contiguous
+
+console.log("array");
+let numbers = [10,21,23,43,321]
+console.log(numbers)
+
+let arr=[];
+console.log(arr);
+
+//arrray object
+let arrObj = new Array(10);
+console.log(arrObj);
+console.log(typeof(arrObj));
+
+console.log(numbers)
+
+// array methods
+numbers.push(60);
+console.log(numbers)
+
+let myarr1=[100,200,300,400,500]
+//array spread operator
+//let myarr2=myarr1  // location is copied (address) (not a copy mechanism)
+//console.log(myarr1);
+//console.log(myarr2);
+//myarr2.push(54);
+//console.log(myarr)   //100 200 300 400 500 54
+let myarr2=[...myarr1];
+myarr2.push(90)
+console.log(myarr2);
+console.log(myarr1)
+
+let deletedElement = myarr1.shift()
+console.log(myarr1)
+
+let newLength = myarr1.unshift(50);
+console.log(newLength)                          // 5
+console.log(myarr1)
+
+let num1 = [10, 20, 30, 40]
+let num2 = [100, 200, 300, 400, 500]
+let combinedArray = num2.concat(num1);
+console.log(combinedArray)                      // 
+
+let dummyArray = [10, "String", true, null, undefined, []]
+console.log(dummyArray)
