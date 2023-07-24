403391061aefbad4313db56717e01d4eff32b0e1
vishal-038 <vk067414@gmail.com>
2023-07-24 10:20:04 +0530

    dom

--- DOM/dom.js
+++ DOM/dom.js
@@ -3,4 +3,6 @@ console.dir(document)
 console.dir(document.URL)
 console.log(document.all)
 
-const myHeading = document
+const myHeading = document.all[4];
+document.all[4].textContent = "welcome to DOM"
+console.log(myHeading)
