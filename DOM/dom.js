28f38dd5591a728f4fe0b80e281491be2a6667cf
vishal-038 <vk067414@gmail.com>
2023-07-25 09:41:13 +0530

    sort and reduce

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,8 +1,9 @@
 // DOM -> Document Object Model
-console.dir(document)
-console.dir(document.URL)
-console.log(document.all)
+// console.dir(document)
+// console.dir(document.URL)
+// console.log(document.all)
 
-const myHeading = document.all[4];
-document.all[4].textContent = "welcome to DOM"
-console.log(myHeading)
+// const myHeading = document.all[4];
+// document.all[4].textContent = "welcome to DOM"
+// console.log(myHeading)
+console.log("hello")
