c7c3f90241341e86cc8f810ddd7f21d86d4bf4d3
vishal-038 <vk067414@gmail.com>
2023-08-01 11:35:45 +0530

    new

--- youtube_applicatoin/main.js
+++ youtube_applicatoin/main.js
@@ -11,7 +11,7 @@
 
 document.getElementById('search-btn').addEventListener('click', () => {
     const text = document.getElementById('search-text').value
-    const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyB3LkS1ru9KjWKLNC7RyrXZeHreSY3hs6M&part=snippet&q=${text}&maxResults=50`
+    const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyC_2CwII3K3NSMxEUzr-XizxknlEvr0N5s&part=snippet&q=${text}&maxResults=50`
     const xhr = new XMLHttpRequest()
     xhr.open('GET', url)
     xhr.onreadystatechange = () => {
@@ -20,8 +20,8 @@ document.getElementById('search-btn').addEventListener('click', () => {
             let output = ''
             for(let i=0; i < response.items.length; i++) {
                 output += `
-                    <a href="https://www.youtube.com/watch?v=${response.items[i].id.videoId}" target="_blank">
                         <div style="width: 23%; margin: 10px;">
+                        <a href="https://www.youtube.com/watch?v=${response.items[i].id.videoId}" target="_blank">
                             <img style="width:100%" src="${response.items[i].snippet.thumbnails.high.url}" />
                             <h4>${response.items[i].snippet.title}</h4>
                             <p>${response.items[i].snippet.description}</p>
